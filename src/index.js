fbbe43b9ee0515db747499e670b6f31ac56ca283
Leonardo Segovia <segovia.leonardo.gustavo@gmail.com>
2018-08-15 23:33:13 -0300

    Add OpenLayers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import WebMap from "./webmap"
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 
-const map = new WebMap('Mapa 1')
-
-map.initialize()
+new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      })
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
